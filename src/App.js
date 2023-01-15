b897c96d590913cf70f31f0c959b487efe4ceec1
mcthuggets <dreu0001@student.monash.edu>
2023-01-15 23:45:45 +0200

    Added some ways to query the search API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
   function App() {
 
@@ -7,19 +8,31 @@ import './App.css';
 
     const clickHandler = () => {
 
-      console.log(searchWord)
-
+      fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyC9FPJkKbHFQDsAGxylisoa99YVFab5yKs&cx=c0da36a615ef44e78&q=' + searchWord)
+        .then( (response) => { console.log(response.json()) })
+        .catch( (error) => { console.log(error.message)})
     }
-
-
-
 
   return (
   <>
 
-    <input type={'text'} placeholder={'Enter you favourite animal here'}
-            onChange={ (event) => { searchWord = event.target.value }} ></input>
-    <button onClick={ clickHandler }> Submit </button>
+    <form>
+
+    <h2> The form below queries the google custom search API and console logs the results.</h2>
+    <p> I couldn't figure out how to use this to display an image because the results do not always contain an image.</p>
+
+    <input  type={'text'} 
+            placeholder={'Enter you favourite animal here'}
+            onChange={ (event) => { searchWord = event.target.value }} 
+            required
+            minLength={2}
+            maxLength={20} >
+    </input>
+
+    <button onClick={ clickHandler } > Submit </button>
+
+    </form>
+
 
   </>
   );
