255d7fec3c5069a701cd76525bcdc0e903998cd7
mcthuggets <dreu0001@student.monash.edu>
2023-01-15 20:44:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+  function App() {
+
+    let searchWord = ''
+
+    const clickHandler = () => {
+
+      console.log(searchWord)
+
+    }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+
+    <input type={'text'} placeholder={'Enter you favourite animal here'}
+            onChange={ (event) => { searchWord = event.target.value }} ></input>
+    <button onClick={ clickHandler }> Submit </button>
+
+  </>
   );
 }
 
